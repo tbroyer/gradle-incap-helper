8fdcc6c99131cfb8a8343d83cc7558de74d739ca
Thomas Broyer <t.broyer@ltgt.net>
2018-09-03 09:36:41 +0200

    Upgrade net.ltgt.errorprone-javacplugin to 0.5

--- buildSrc/src/main/kotlin/local/java-library.gradle.kts
+++ buildSrc/src/main/kotlin/local/java-library.gradle.kts
@@ -2,6 +2,7 @@ package local
 
 plugins {
     `java-library`
+    id("net.ltgt.errorprone-javacplugin")
 }
 
 java.sourceCompatibility = JavaVersion.VERSION_1_8
@@ -11,18 +12,13 @@ if (JavaVersion.current().isJava9Compatible) {
     }
 }
 
-if (JavaVersion.current().isJava9Compatible) {
-    apply(plugin = "net.ltgt.errorprone-javacplugin")
-} else {
-    apply(plugin = "net.ltgt.errorprone")
-}
-
 repositories {
     mavenCentral()
 }
 
 dependencies {
     "errorprone"("com.google.errorprone:error_prone_core:2.3.1")
+    "errorproneJavac"("com.google.errorprone:javac:9+181-r4173-1")
 }
 
 val javadoc by tasks.getting(Javadoc::class) {
